6b5a759424dbd1a40b640a81a7f4abfa58d0964c
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2024-05-13 10:23:38 +0200

    example with modules

---
+++ Livekod/my-first-express-server/operations.js
@@ -0,0 +1,8 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+// Detta är commonJS-sättet
+// module.exports = { add }
+
+//Detta är ES6-sättet
+export default add;
--- Livekod/my-first-express-server/server.js
+++ Livekod/my-first-express-server/server.js
@@ -1,16 +1,34 @@
-const express = require('express');
-const path = require('path');
 
+// Server med ES6-moduler
+import express from 'express';
+import add from './operations.js';
 const app = express();
+
 const PORT = 8000;
 
-const dirPath = path.join(__dirname, 'public');
-app.use(express.static(dirPath));
-
-app.get('/', (request, response) => {
-    response.sendFile(path.join(dirPath, 'index.html'));
+app.get("/", (req, res) => {
+    console.log(add(5, 6));
+    res.send("Hejsan Hemsidan!");
 })
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}...`);
-});
+})
+
+// Server med CommonJS-moduler
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+// const PORT = 8000;
+
+// const dirPath = path.join(__dirname, 'public');
+// app.use(express.static(dirPath));
+
+// app.get('/', (request, response) => {
+//     response.sendFile(path.join(dirPath, 'index.html'));
+// })
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}...`);
+// });
